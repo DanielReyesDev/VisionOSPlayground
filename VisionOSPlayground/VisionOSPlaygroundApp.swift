f09745076fcccc2cf662340bf495d6aa978ba282
Daniel Reyes <vn0e21f@email.wal-mart.com>
2023-06-23 15:23:30 -0600

    Initial Commit

---
+++ VisionOSPlayground/VisionOSPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOSPlaygroundApp.swift
+//  VisionOSPlayground
+//
+//  Created by Daniel Reyes on 23/06/23.
+//
+
+import SwiftUI
+
+@main
+struct VisionOSPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.windowStyle(.volumetric)
+    }
+}
